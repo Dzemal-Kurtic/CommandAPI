c401fe82896c68f4c9b8cee65dfac53453f82d35
Džemal Kurtić <dzemaludeen@gmail.com>
2020-12-10 07:02:41 +0100

    Added DTO

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Npgsql;
+using AutoMapper;
 
 namespace CommandAPI
 {
@@ -34,6 +35,7 @@ namespace CommandAPI
             services.AddControllers();
             services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
